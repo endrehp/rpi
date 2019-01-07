182272fb9d7b5fd89649ffa21d36409ee37746f3
Endre Hauge Paulsen <endrehp@stud.ntnu.no>
2019-01-07 13:12:02 +0100

    client function

--- ServerSocket2/main.c
+++ ServerSocket2/main.c
@@ -6,7 +6,31 @@
 
 int main(){
 
+    // create a socket
+    int network_socket;
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+    // specify an address for the socket
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY; // connecting to local machine 0.0.0.0
 
+    int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    // check for error with the connection
+    if (connection_status == -1) {
+        printf("There was an error making a connection to the remote socket\n\n")
+    }
+
+    // receive data from the server
+    char server_response[256];
+    recv(network_socket, server_response, sizeof(server_response), 0);
+
+    // Print out the server's response
+    printf("The server sent the data: %s\n", server_response);
+
+    // close the socket
+    close(network_socket);
     return 0;
 }
